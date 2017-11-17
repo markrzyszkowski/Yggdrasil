8efa167726f8b82fd0b0894bfd6daadb2315e2ce
Michal Matuszewski <mszmatuszewski@gmail.com>
2017-11-17 13:16:29 +0100

    Removed Spring Shell dependency

--- src/test/java/com/github/asgardbot/StartContextTest.java
+++ src/test/java/com/github/asgardbot/StartContextTest.java
@@ -1,18 +1,15 @@
 package com.github.asgardbot;
 
-import com.github.asgardbot.shell.TestShellRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Import(TestShellRunner.class)
 public class StartContextTest {
 
     @Test
-    public void contextLoads() {}
+    public void main_context_shouldStart() {}
 }
 
