298c0c73ec494cf4ccbb98d3816a07f1b04f5762
Michał Matuszewski <mszmatuszewski@users.noreply.github.com>
2017-11-20 17:56:06 +0100

    Removed redundant code

--- src/main/java/com/github/asgardbot/nlp/NlpResolver.java
+++ src/main/java/com/github/asgardbot/nlp/NlpResolver.java
@@ -3,12 +3,8 @@ package com.github.asgardbot.nlp;
 import com.github.asgardbot.commons.ServiceId;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Component
 public class NlpResolver {
-
-    Map<String, ServiceId> commands;
 
     public ServiceId resolve(String payload) {
         return new ServiceId("placeholder");
